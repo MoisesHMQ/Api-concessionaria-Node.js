6b404e1c4ce24590661448491aa2afabdede56b4
MoisesHMQ <mendes.moisesmgs@gmail.com>
2022-07-29 18:56:20 -0300

    listar clientes

--- api_concessionaria.js
+++ api_concessionaria.js
@@ -83,3 +83,8 @@ app.post('/login/cliente', (request, response) => {
             return response.send("Erro: cpf ou Senha incorretos.");
         }
     })
+
+app.get('/listar/cliente', (request, response) => {
+    console.log(request.body);
+    return response.json(cliente)
+})
